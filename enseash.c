0ed115a03dedf3ba035375e07cde667d2f14ad03
osmane-amine <osmane.parcoursup@gmail.com>
2025-12-08 09:22:06 +0100

    1.
    better syntax

--- enseash.c
+++ enseash.c
@@ -1,11 +1,19 @@
 
+#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 
 
-
-
-
-int main(int argv, char *argc){
-	write(STDOUT_FILENO, "$ ./enseash\nBienvenue dans le Shell ENSEA.\nPour quitter, tapez 'exit'.\nenseash % \n", 83);
+int main(int argv, char **argc){
+	char* buffer;
+	int max_len = 1024;
+	if(argv != 1) {
+		buffer = "wrong number of arguments";
+		write(STDERR_FILENO, buffer, strnlen(buffer,max_len));
+		return EXIT_FAILURE;
+	}
+	buffer = "$ ./enseash\nBienvenue dans le Shell ENSEA.\nPour quitter, tapez 'exit'.\nenseash %";
+	write(STDOUT_FILENO, buffer, strnlen(buffer,max_len));
+	
+	return EXIT_SUCCESS;
 }
