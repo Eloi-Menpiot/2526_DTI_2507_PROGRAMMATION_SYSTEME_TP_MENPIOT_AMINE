492dadd0615ad9cbcb2a4fdd3d82d2922bc5b54d
Eloi-Menpiot <eloi.menpiot@gmail.com>
2025-12-08 11:53:58 +0100

    2

--- enseash2.c
+++ enseash2.c
@@ -1,45 +1,50 @@
 
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 
-#define BUFSIZE 1024
-
+#define MAX_LEN 1024
 
 int main(int argv, char **argc){
 	char* buffer;
 	if(argv != 1) {
-		buffer = "wrong number of arguments";
-		write(STDERR_FILENO, buffer, strnlen(buffer,BUFSIZE));
+		buffer = "Wrong number of arguments";
+		write(STDERR_FILENO, buffer, strnlen(buffer, MAX_LEN));
 		return EXIT_FAILURE;
 	}
-	buffer = "$ ./enseash\nBienvenue dans le Shell ENSEA.\nPour quitter, tapez 'exit'.\nenseash % ";
-	write(STDOUT_FILENO, buffer, strnlen(buffer,BUFSIZE));
-	while (1) {
-		char input[BUFSIZE] = "";
-		read(STDIN_FILENO, input, BUFSIZE);
+	buffer = "$ ./enseash\nBienvenue dans le Shell ENSEA.\nPour quitter, tapez 'exit'.\nenseash %";
+	write(STDOUT_FILENO, buffer, strnlen(buffer, MAX_LEN));
 
-		char* token = strtok(input," \n");
-		if (token != NULL) {
-			if (strncmp(token, "exit", BUFSIZE) == 0) {
+	while(1){
+		char input[MAX_LEN] = "";
+		read(STDIN_FILENO, input, MAX_LEN);
+
+		char * token = strtok(input, " \n");
+		if (token != NULL){
+
+			if(strncmp(token, "exit", MAX_LEN) == 0){
 				buffer = "A bientôt !\n";
-				write(STDOUT_FILENO, buffer, strnlen(buffer,BUFSIZE));
+				write(STDOUT_FILENO, buffer, strnlen(buffer, MAX_LEN));
 				return EXIT_SUCCESS;
 			}
+
 			int pid, status;
-			if ((pid = fork()) != 0) {
-				wait(&status);
-				//print ?
-				buffer = "\nenseash % ";
-				write(STDOUT_FILENO, buffer, strnlen(buffer,BUFSIZE));
-			}else {
-				execlp(token, token, (char*)NULL);
-				buffer = strncat("Error while running ",token, BUFSIZE);
-				write(STDERR_FILENO, buffer, strnlen(buffer,BUFSIZE));
-				exit(EXIT_FAILURE);
+			if ((pid = fork()) != 0){
+                wait(&status);
+                buffer = "\nenseash % ";
+                write(STDOUT_FILENO, buffer, strnlen(buffer, MAX_LEN));
+			}
+
+			else{
+				execlp(token , token, (char*)NULL);
+                buffer = strncat("Error while running", buffer, strnlen(buffer, MAX_LEN));
+                write(STDOUT_FILENO, buffer, strnlen(buffer, MAX_LEN));
+                exit(EXIT_FAILURE);
 			}
 		}
 	}
+
+	return EXIT_SUCCESS;
 }
