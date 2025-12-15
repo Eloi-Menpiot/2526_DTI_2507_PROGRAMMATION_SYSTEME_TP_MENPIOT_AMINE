ab4ee775cb6220e5fd3f0246ddfceaabc0ed7daa
Eloi-Menpiot <eloi.menpiot@gmail.com>
2025-12-15 11:13:23 +0100

    Standardisation des définitions avec Osmane

--- enseash.c
+++ enseash.c
@@ -3,17 +3,17 @@
 #include <unistd.h>
 #include <string.h>
 
-#define MAX_LEN 1024
+#define BUFSIZE 128
 
 int main(int argv, char **argc){
 	char* buffer;
 	if(argv != 1) {
 		buffer = "wrong number of arguments";
-		write(STDERR_FILENO, buffer, strnlen(buffer, MAX_LEN));
+		write(STDERR_FILENO, buffer, strnlen(buffer, BUFSIZE));
 		return EXIT_FAILURE;
 	}
 	buffer = "$ ./enseash\nBienvenue dans le Shell ENSEA.\nPour quitter, tapez 'exit'.\nenseash %";
-	write(STDOUT_FILENO, buffer, strnlen(buffer, MAX_LEN));
+	write(STDOUT_FILENO, buffer, strnlen(buffer, BUFSIZE));
 	
 	return EXIT_SUCCESS;
 }
--- enseash2.c
+++ enseash2.c
@@ -5,7 +5,7 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 
-#define MAX_LEN 1024
+#define BUFSIZE 128
 #define CHILD 0
 #define EXIT_QUERY "exit"
 
@@ -13,22 +13,22 @@ int main(int argv, char **argc){
 	char* buffer;
 	if(argv != 1) {
 		buffer = "Wrong number of arguments";
-		write(STDERR_FILENO, buffer, strnlen(buffer, MAX_LEN));
+		write(STDERR_FILENO, buffer, strnlen(buffer, BUFSIZE));
 		return EXIT_FAILURE;
 	}
 	buffer = "$ ./enseash\nBienvenue dans le Shell ENSEA.\nPour quitter, tapez 'exit'.\nenseash %";
-	write(STDOUT_FILENO, buffer, strnlen(buffer, MAX_LEN));
+	write(STDOUT_FILENO, buffer, strnlen(buffer, BUFSIZE));
 
 	while(1){
-		char input[MAX_LEN] = "";
-		read(STDIN_FILENO, input, MAX_LEN);
+		char input[BUFSIZE] = "";
+		read(STDIN_FILENO, input, BUFSIZE);
 
 		char * token = strtok(input, " \n");
 		if (token != NULL){
 
-			if(strncmp(token, EXIT_QUERY, MAX_LEN) == 0){
+			if(strncmp(token, EXIT_QUERY, BUFSIZE) == 0){
 				buffer = "A bientôt !\n";
-				write(STDOUT_FILENO, buffer, strnlen(buffer, MAX_LEN));
+				write(STDOUT_FILENO, buffer, strnlen(buffer, BUFSIZE));
 				return EXIT_SUCCESS;
 			}
 
@@ -36,13 +36,13 @@ int main(int argv, char **argc){
 			if ((pid = fork()) != 0){
                 wait(&status);
                 buffer = "\nenseash % ";
-                write(STDOUT_FILENO, buffer, strnlen(buffer, MAX_LEN));
+                write(STDOUT_FILENO, buffer, strnlen(buffer, BUFSIZE));
 			}
 
 			else{
 				execlp(token , token, (char*)NULL);
-                buffer = strncat("Error while running", buffer, strnlen(buffer, MAX_LEN));
-                write(STDOUT_FILENO, buffer, strnlen(buffer, MAX_LEN));
+                buffer = strncat("Error while running", buffer, strnlen(buffer, BUFSIZE));
+                write(STDOUT_FILENO, buffer, strnlen(buffer, BUFSIZE));
                 exit(EXIT_FAILURE);
 			}
 		}
