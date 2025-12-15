6d4ca1563252e16382fa127dab5d2096d842f133
Eloi-Menpiot <eloi.menpiot@gmail.com>
2025-12-15 11:01:00 +0100

    Ajout du temps d'exécution

---
+++ enseash5.c
@@ -0,0 +1,59 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+
+#define BUFSIZE 128
+#define CHILD 0
+
+
+int main(void){
+	char buffer[BUFSIZE]= "$ ./enseash\nBienvenue dans le Shell ENSEA.\nPour quitter, tapez 'exit'.\nenseash % ";
+	write(STDOUT_FILENO, buffer, strnlen(buffer,BUFSIZE));
+	while (1) {
+		char input[BUFSIZE] = "";
+		read(STDIN_FILENO, input, BUFSIZE);
+
+		char* token = strtok(input," \n");
+		if (token != NULL) {
+			if (strncmp(token, "exit", BUFSIZE) == 0) {
+				strncpy(buffer, "A bientôt !\n", BUFSIZE);
+				write(STDOUT_FILENO, buffer, strnlen(buffer,BUFSIZE));
+				return EXIT_SUCCESS;
+			}
+
+			struct timespec timer_start;
+			struct timespec timer_end;
+			clock_gettime(CLOCK_REALTIME, &timer_start);
+
+			int pid, status;
+			if ((pid = fork()) != CHILD) { // Création de l'enfant
+				wait(&status);
+				clock_gettime(CLOCK_REALTIME, &timer_end);
+                int delay = timer_end.tv_sec*1e3 - timer_start.tv_sec*1e3 + timer_end.tv_nsec*1e-6 - timer_start.tv_nsec*1e-6;
+
+				if (WIFEXITED(status)) {  // If child stops normally
+					snprintf(buffer, BUFSIZE,"\nenseash [exit : %d|%dms] %%", WEXITSTATUS(status), delay);
+				}
+                
+                else if (WIFSIGNALED(status)) { // If signal stops child
+					snprintf(buffer, BUFSIZE,"\nenseash [sign : %d] %%", WTERMSIG(status), delay);
+				}
+
+				write(STDOUT_FILENO, buffer, strnlen(buffer,BUFSIZE));
+
+			}
+            
+            else { 
+				execlp(token, token, (char*)NULL);
+				sprintf(buffer, "Error while running %s",token);
+				write(STDERR_FILENO, buffer, strnlen(buffer,BUFSIZE));
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
+}
